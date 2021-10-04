36413e4b9b16cd2f213290925c5439aacfa18dc2
Kevin Bequoye <beq.kevin@gmail.com>
2021-10-04 14:49:23 +0200

    Opstarten oefening

---
+++ js/kleurplaat.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+class Kleurplaat {
+    #kleurplaat;
+    constructor(kleurplaat) {
+        this.#kleurplaat = kleurplaat;
+    }
+
+}
---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const kleurplaat = document.getElementById("kleurplaat");
+const knopRood = document.getElementById("rood");
+const knopGroen = document.getElementById("groen");
+const knopBlauw = document.getElementById("blauw");
+const knopReset = document.getElementById("wit");
+
+
+kleurplaat.innerText = "Rarara";
+
+knopRood.addEventListener("click", () => {
+    
+});
